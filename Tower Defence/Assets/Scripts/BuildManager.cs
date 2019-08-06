6e5a039c77d001acb10c53139f41883ef61df1cc
Batooon <rozum.ant@gmail.com>
2019-08-06 16:57:46 +0300

    Now you can place turrets

---
+++ Tower Defence/Assets/Scripts/BuildManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+
+    public static BuildManager instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("More than one BuildManager in scene!");
+            return;
+        }
+        instance = this;
+    }
+
+    public GameObject standartTurretPrefab;
+
+    void Start()
+    {
+        turretToBuild = standartTurretPrefab;
+    }
+
+    private GameObject turretToBuild;
+
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+}
---
+++ Tower Defence/Assets/Scripts/Platform.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public Color hoverColor;
+    public Vector3 positionOffset;
+
+    private GameObject turret;
+
+    private Renderer rend;
+    private Color startColor;
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+    void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+
+    void OnMouseDown()
+    {
+        if (turret != null)
+        {
+            Debug.Log("WTF NIGGA ARE YOU DUMB?? tHeRe IS ALREAADY A TURRET!");
+            return;
+        }
+
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = (GameObject)Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}
