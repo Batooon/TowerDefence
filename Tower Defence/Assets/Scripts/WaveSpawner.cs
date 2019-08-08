4e773db14ef16f3501221b9530f9945cb2319c7b
Batooon <rozum.ant@gmail.com>
2019-08-08 20:37:13 +0300

    Minor fixes

--- Tower Defence/Assets/Scripts/WaveSpawner.cs
+++ Tower Defence/Assets/Scripts/WaveSpawner.cs
@@ -15,7 +15,7 @@ public class WaveSpawner : MonoBehaviour
 
     public bool isWaveIncoming;
 
-    public float countdown = 10f;
+    public float countdown;
     private float spawnBetweenEnemies = 0.5f;
 
     public Text wavecountdownText;
@@ -25,7 +25,7 @@ public class WaveSpawner : MonoBehaviour
     {
         if (countdown <= 0f)
         {
-            if (waveNumber < waves.Length)
+            if (waveNumber < waves.Length && !isWaveIncoming)
             {
                 isWaveIncoming = true;
                 StartCoroutine(SpawnWave());
@@ -34,7 +34,10 @@ public class WaveSpawner : MonoBehaviour
         }
 
         countdown -= Time.deltaTime;
-        wavecountdownText.text = Mathf.CeilToInt(countdown).ToString();
+        if (!isWaveIncoming)
+            wavecountdownText.text = Mathf.CeilToInt(countdown).ToString();
+        else
+            wavecountdownText.text = 0.ToString();
     }
 
     private IEnumerator SpawnWave()
@@ -44,6 +47,8 @@ public class WaveSpawner : MonoBehaviour
             SpawnEnemy(i);
             yield return new WaitForSeconds(spawnBetweenEnemies);
         }
+        yield return new WaitForSeconds(10f);//TODO: сделать проверку, когда волна заканчивается и врагов нет на поле, чтобы начать отсчет до новой волны
+        isWaveIncoming = false;
         waveNumber++;
     }
 
