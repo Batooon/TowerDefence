195c730fdb32e4d251570fcefa9530cae404a95a
Anton <rozum.ant@gmail.com>
2019-11-10 13:53:12 +0200

    Editor script for resetting Player Prefs

---
+++ Tower Defence/Assets/Scripts/EditorScripts/ExtendedTools.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class ExtendedTools : EditorWindow
+{
+    [MenuItem("Window/ExTools")]
+    public static void ShowWindow()
+    {
+        GetWindow<ExtendedTools>("My Tools");
+    }
+
+    void OnGUI()
+    {
+        if(GUILayout.Button("Reset PlayerPrefs"))
+        {
+            PlayerPrefs.DeleteAll();
+        }
+    }
+}
