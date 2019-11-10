264226489a71f711f659821009b31388f547f374
Anton <rozum.ant@gmail.com>
2019-11-10 13:48:10 +0200

    Level Changer added

---
+++ Tower Defence/Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+    public Animator animator;
+
+    private int levelToLoad;
+
+    void Start()
+    {
+        Game.singleton.ChangeLevel += FadeToLevel;
+    }
+
+    public void FadeToLevel(int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
