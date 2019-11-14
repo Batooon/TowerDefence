6f996b9c320b87ee994631720a23d3798adf225f
Anton <rozum.ant@gmail.com>
2019-11-14 23:32:25 +0200

    Added sound manager

---
+++ Tower Defence/Assets/AudioManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager singleton;
+
+    public Sound[] sounds;
+
+    void Awake()
+    {
+        if (singleton == null)
+            singleton = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            sounds[i].source = gameObject.AddComponent<AudioSource>();
+
+            sounds[i].source.clip = sounds[i].clip;
+            sounds[i].source.volume = sounds[i].volume;
+            sounds[i].source.pitch = sounds[i].pitch;
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning($"Sound: {name} not found!");
+            return;
+        }
+        s.source.Play();
+    }
+}
